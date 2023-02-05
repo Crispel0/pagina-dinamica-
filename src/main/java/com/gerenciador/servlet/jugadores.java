fe7d65d490bdaab7b8d1a04f7c54687d52d7995a
Crispelo <penagoscristian61@gmail.com>
2023-02-05 12:24:05 -0500

    getRequestDistpacher and forward with the parameter of httpRequest

--- src/main/java/com/gerenciador/servlet/jugadores.java
+++ src/main/java/com/gerenciador/servlet/jugadores.java
@@ -1,5 +1,6 @@
 package com.gerenciador.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -23,8 +24,18 @@ public class jugadores extends HttpServlet {
 		
 		jugadores.setNombre(nombreJugador);
 		
+		DB basedatos = new DB();
+		basedatos.agregarjugadores(jugadores);
 		
+		//send to JSP
+		//receive all the information about of my request and comunicate between the field java jugadores and jpss
+		RequestDispatcher rd =  request.getRequestDispatcher("/nuevoListaJugadores.jsp");
 		
+		/*send to all acquisition of a request and a response*/
+		//for middle of this setAttribute I can access to the parameter of of my method//
+		request.setAttribute("nombreJugador", jugadores.getNombre());
+		rd.forward(request, response);
+			
 	}
 
 
