13763c3c5e6b69a2f0858ef4258cb0b09decd60f
Crispelo <penagoscristian61@gmail.com>
2023-02-15 12:47:13 -0500

    add servlet of eliminarJugadores

--- src/main/java/com/gerenciador/servlet/EliminarJugadores.java
+++ src/main/java/com/gerenciador/servlet/EliminarJugadores.java
@@ -1,5 +1,26 @@
 package com.gerenciador.servlet;
 
-public class EliminarJugadores {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/eliminarJugadores")
+public class eliminarJugadores extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String paramId = request.getParameter("id");
+		Integer id = Integer.valueOf(paramId);
+		
+		DB db = new DB();
+		db.eliminarJugadores(id);
+		
+		response.sendRedirect("listaJudadores");
+		
+	}
 
 }
